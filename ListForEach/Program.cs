d1976e1dbcaa80a0a711bd019814fc2caf9b215c
GaweVerl <79454924+GaweVerl@users.noreply.github.com>
2021-03-19 08:46:18 +0100

    Add project files.

---
+++ ListForEach/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListForEach
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Wat wil je doen? \n1. Studentengegevens invoeren\n2. Studentengegevens tonen");
+            int keuze = Convert.ToInt32(Console.ReadLine());
+
+            //  switch (keuze)
+            {
+
+                //  case 1:
+                {
+                    Console.WriteLine("Voor welke student wil je de gegevens invullen? Geef een cijfer van 1 tot 5 op.");
+                    int student = Convert.ToInt32(Console.ReadLine());
+                    List<Student> mijnStudenten = new List<Student>();
+
+
+
+
+                    switch (student)
+                    {
+                        case 1:
+                            Console.WriteLine("Naam: \n Leeftijd: \nPunten Communicatie: \nPunten PP: \nPunten WT: ");
+                            mijnStudenten.Add(new Student() { Naam = Console.ReadLine(), Leeftijd = Convert.ToInt32(Console.ReadLine()), PuntenComm = Convert.ToInt32(Console.ReadLine()), PuntenPP = Convert.ToInt32(Console.ReadLine()), PuntenWT = Convert.ToInt32(Console.ReadLine()) });
+
+                            //                  List<Student>.ForEach(item => Console.Write(mijnStudenten));
+
+
+
+                            
+                            Console.WriteLine($"1: {mijnStudenten[0]}");
+                            Console.ReadLine();
+
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Naam: \n Leeftijd: \nPunten Communicatie: \nPunten PP: \nPunten WT: ");
+                            mijnStudenten.Add(new Student() { Naam = Console.ReadLine(), Leeftijd = Convert.ToInt32(Console.ReadLine()), PuntenComm = Convert.ToInt32(Console.ReadLine()), PuntenPP = Convert.ToInt32(Console.ReadLine()), PuntenWT = Convert.ToInt32(Console.ReadLine()) });
+
+                            Console.WriteLine($"1: {mijnStudenten[0]}");
+                            Console.ReadLine();
+
+                            break;
+
+                        
+                    }
+
+
+                    //                case 3:
+
+                    mijnStudenten.Add(new Student() { Naam = "Ken" });
+                    //                  break;
+                    //               case 4:
+
+                    mijnStudenten.Add(new Student() { Naam = "Ken" });
+                    //                   break;
+                    //               case 5:
+
+                    mijnStudenten.Add(new Student() { Naam = "Ken" });
+                    //                    break;
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
+
+
+
---
+++ ListForEach/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListForEach
+{
+
+    enum Klassen { EA1, EA2, EA3, EA4 }
+    class Student
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Klassen Klas { get; set; }
+
+        public int PuntenComm { get; set; }
+        public int PuntenPP { get; set; }
+        public int PuntenWT { get; set; }
+
+        public double BerekTotCijf()
+        {
+        return (PuntenComm + PuntenPP + PuntenWT)/3.0;
+            }
+
+        public void GeefOverzicht()
+        {
+            Console.WriteLine($"{Naam}, { Leeftijd} jaar");
+            Console.WriteLine($"Klas: {Klas}");
+            Console.WriteLine();
+            Console.WriteLine("Cijferrapport");
+            Console.WriteLine("***************");
+            Console.WriteLine($"Communicatie:\t\t{PuntenComm}");
+            Console.WriteLine($"PP: \t\t {PuntenPP}");
+            Console.WriteLine($"Web Tech: \t\t{PuntenWT}");
+            Console.WriteLine($"Gemiddelde:\t\t{BerekTotCijf():0.0}");
+
+        }
+
+    }
+}
